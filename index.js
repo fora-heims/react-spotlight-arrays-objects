c7b09ebb9f7862fa241033c2823a09dcf422e599
fora-heims <foraheims@gmail.com>
2021-12-02 13:15:54 -0800

    one more done. nested rest arrays.

--- index.js
+++ index.js
@@ -41,14 +41,17 @@ export const printUserInfo = ({
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {};
+export const getSum = ({ ...rest }) => {
+  let sum = 0;
+  rest.forEach((num) => (sum += num));
+};
 
 // INPUT: an unknown number of arguments
 // OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
 // REQS: use rest parameters
 // getFirstTwoArgs(1, 2, 3, 4, 5) should return [1, 2, [3, 4, 5]]
 // getFirstTwoArgs('a', 'b', 'c', 'd') should return ['a', 'b', ['c', 'd']]
-export const getFirstTwoArgs = () => {};
+export const getFirstTwoArgs = (a, b, ...rest) => [a, b, rest];
 
 // INPUT: an object with the following structure
 // {
